eb6664290f793af0e4cc0d4741f40ecd116fa71a
johannesnussbaum <johannes.nussbaum@zoho.com>
2020-03-11 12:27:10 +0100

    PingPongServer.java aktualisiert
    Former-commit-id: 3026cdbd45887103f2dcf22e7836128b1b5af851

--- src/main/java/tbc/server/PingPongServer.java
+++ src/main/java/tbc/server/PingPongServer.java
@@ -1,54 +1,58 @@
 package tbc.server;
 
-import java.io.IOException;
-import java.net.*;
-import java.util.Calendar;
-
 public class PingPongServer implements Runnable {
 
-    private Socket socket;
-    private boolean PingPongFailed;
-    private long lastFail = 0;
+    /**
+     * Object fields:
+     * sendPing: Received by server.
+     * numOfClients: Received by server.
+     * clientsConnectivity: Each position i represents client i. Each pong received by that client adds 1 to the
+     *      respective position. The sum on position i represents the number of successful ping/pongs.
+     * pingFrequency: How many times per second that a ping should be sent.
+     */
+    boolean sendPing;
+    int numOfClients;
+    private int[] clientsConnectivity = new int[100];
+    int pingFrequency = 10;
 
     /**
-     * @param socket: Pass the already existing socket to the PingPong-Thread, enabling the PingPong-
-     *              Thread to communicate via this socket.
-     * @param PingPongFailed: First create this boolean in your Server with the value "false",
-     *                      and then pass it to the PingPong-Thread. The PingPong-Thread will swap it
-     *                      to "true" as soon as ...
-     *                      You should watch this boolean within the class which calls
-     *                      this constructor.
+     * @param sendPing: Object field of the server, initially false. Please pass the field itself to PingPongServer,
+     *                not only its value.As soon as PingPongServer swaps it to true,
+     *                the server sends a ping to all clients via network protocol. Then, the server resets sendPing
+     *                to false. Every access to sendPing has to be synchronized!
+     * @param numOfClients: Object field of the server. Please pass the field itself to PingPongServer, not only its
+     *                    value.
      */
-    public PingPong(Socket socket, boolean PingPongFailed) {
-        this.socket = socket;
-        this.PingPongFailed = PingPongFailed;
-    }
-
-    private static class ConnectionTestException extends IOException {
-        long timeInMillis;
-        ConnectionTestException(long timeInMillis) {
-            this.timeInMillis = timeInMillis;
-        }
+    public PingPongServer(boolean sendPing, int numOfClients) {
+        this.sendPing = sendPing;
+        this.numOfClients = numOfClients;
     }
 
     @Override
     public void run() {
-        while(socket.isConnected()) {
+        //pingFrequency times per second, sendPing is set to true in order to make the server send a ping to all clients.
+        for (int i = 0; i < pingFrequency; i++) {
+            sendPing = true;    //Wie mache ich das synchronized?
             try {
-                socket.sendUrgentData(90);
-                Thread.sleep((long) 100);
-            } catch (IOException e) {
-                long thisFail = Calendar.getInstance().getTimeInMillis();
-                if (thisFail - lastFail >= 1000) { //everything is okay
-                    lastFail = thisFail;
-                } else {
-
-                }
+                Thread.sleep((long) (1000 / pingFrequency));
             } catch (InterruptedException e) { /*do nothing*/
-            } catch (ConnectionTestException e) {
-
             }
-
         }
+
+        //after 1 second, PingPongServer checks the sums of clientsConnectivity. If one client's sum is less than 9
+        // (i.e., 2 or more ping/pongs failed), the server will be notified with connectionBroken(int numOfClient)
+        for (int i = 0; i < numOfClients; i++) {
+            if (clientsConnectivity[i] < 9) {
+                Server.connectionBroken(i);     //Wie adressiere ich den Server?
+            }
+        }
+    }
+
+    /**
+     * Each pong received by client numOfClient adds 1 to the sum of successful ping/pongs
+     * at clientsConnectivity[numOfClient].
+     */
+    void pongArrived(int numOfClient) {
+        clientsConnectivity[numOfClient]++;
     }
 }
