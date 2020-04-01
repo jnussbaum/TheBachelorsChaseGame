03d1e797f0020e9bff3250508cdb48962cc45785
Adrian <adrian.prokopczyk@stud.unibas.ch>
2020-04-01 09:08:21 +0200

    Hinzufügung der Player Klasse
    Former-commit-id: bf67046942549d1c61f2df120ccf316b86ec8bb1

---
+++ src/main/java/tbc/game/Player.java
@@ -0,0 +1,69 @@
+package tbc.game;
+
+import java.util.ArrayList;
+import tbc.server.ServerHandler;
+
+public class Player {
+  boolean tooMuchPoints = false;
+  String name;
+  int numOfPoints;
+  int numOfCoins;
+  int numOfCards;
+  ServerHandler serverHandler;
+  ArrayList<Card> cards;
+
+  public Player(String name) {
+    this.name = name;
+  }
+
+  public void setServerHandler(ServerHandler serverHandler) {
+    this.serverHandler = serverHandler;
+  }
+
+  public ServerHandler getServerHandler() {
+    return serverHandler;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setNumOfPoints(int numOfPoints) {
+    this.numOfPoints = numOfPoints;
+  }
+
+  public void setNumOfCoins(int numOfCoins) {
+    this.numOfCoins = numOfCoins;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getNumOfPoints() {
+    return numOfPoints;
+  }
+
+  public int getNumOfCoins() {
+    return numOfCoins;
+  }
+
+  public ArrayList<Card> getCards() {
+    return cards;
+  }
+
+  /**
+   * Calculates the Points the Player through the given Carts
+   */
+
+  int calculatePoints() {
+      //Iterate through a client's cards
+      int sum = 0;
+      for (Card c : cards) {
+        sum += c.getValue();
+      }
+      setNumOfPoints(sum);
+      return sum;
+    }
+
+}
