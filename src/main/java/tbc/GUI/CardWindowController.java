0077693c947e92429d44d28b5ecf5fe54e6c8e11
annadiack <anna.diack@stud.unibas.ch>
2020-04-04 19:21:04 +0200

    Logger in Karten-fenster eingefügt.
    Former-commit-id: 5b6a8845b2b36f3f5c33e25a6dfe2004f0dd06e6

--- src/main/java/tbc/GUI/CardWindowController.java
+++ src/main/java/tbc/GUI/CardWindowController.java
@@ -4,16 +4,18 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class CardWindowController {
 
-
+  private static final Logger LOGGER = LogManager.getLogger(CardWindowController.class);
   @FXML private Button btnZurueck;
 
 
   public void closeCardWindow() {
     Stage stage = (Stage) btnZurueck.getScene().getWindow();
     stage.close();
-    System.out.println("Closed cards window.");
+    LOGGER.info("Closed cards window.");
   }
 }
