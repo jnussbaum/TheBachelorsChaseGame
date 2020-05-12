5c20b9dc0ee634a46b0a28c23d31d6b8fb2df527
annadiack <anna.diack@stud.unibas.ch>
2020-05-12 13:51:11 +0200

    Fehler in der addCard() wieder entfernt.
    Former-commit-id: f9449c35a9b986ab9a9e0c0258e8692ac33e5253

--- src/main/java/tbc/client/ClientGame.java
+++ src/main/java/tbc/client/ClientGame.java
@@ -41,8 +41,7 @@ public class ClientGame {
      * @param cardName The new card, which the user got, will be appended to the ArrayList 'cards'.
      */
     public void addCard(String cardName) {
-        if(cardName.contains("Cheat"))
-        cards.add(Card.valueOf(cardName));
+            cards.add(Card.valueOf(cardName));
         Platform.runLater(
             () -> {
                 LobbyController.gameWindowController
